1afc98d2e34d9869ebfbce7fdda5085581cab063
liangjuan1105 <1459722320@qq.com>
2019-06-06 00:10:49 +0800

    TDFcloudindex

--- index/src/components/page/TdfCloudIndex.vue
+++ index/src/components/page/TdfCloudIndex.vue
@@ -1,24 +1,116 @@
 <template>
   <div class="TDFCloudIndex">
     <div class="banner">
-       <el-row :gutter="10">
-         <el-col :span="12"><div class="banner-font">太极微服务开发框架TDF Cloud</div></el-col>
-       </el-row>
-       <!--<span class="span-font">太极微服务开发框架TDF Cloud</span>-->
-       <!--<div class="span-font2">简要的概括产品的定位、特性。</div>-->
-       <!--<div class="button">-->
-         <!--<el-button>开始使用</el-button>-->
-       <!--</div>-->
+       <div class="div1">
+           太极微服务开发框架TDF Cloud
+       </div>
+       <div class="div2">
+         简要的概括产品的定位、特性。
+       </div>
+       <div class="button">
+         <el-button>开始使用</el-button>
+       </div>
       <!--<div class="img">-->
         <!--<span><img src="http://tech.taiji.com.cn/community/openSourceProjDetail?linkid=0bbc52dbc151419d866e0c89b5baa253" alt="TDF cloud"></span>-->
       <!--</div>-->
+    </div>
+    <div class="indexPic">
+      <img src="http://tech.taiji.com.cn/community/openSourceProjDetail?linkid=0bbc52dbc151419d866e0c89b5baa253" alt="TDF cloud">
+    </div>
+    <div class="index">
+      <div>
+        <el-row>
+          <el-col :span="24" class="indexNav">
+            <el-tabs class="indexNav1">
+              <el-tab-pane label="产品概览">
+                <div class="content">
+                  <h3 class="content_title">太极微服务平台 TDF Cloud简介</h3>
+                  <p class="content_detail">太极微服务平台(TDF Cloud)是一个围绕应用和微服务的Paas平台，<br/>提供一站式应用全生命周期管理能力和数据化运营支持，提供多维<br/>度应用和服务的监控数据，助力服务性能优化，提供基于Spring <br/>Cloud的微服务架构的技术支持。</p>
+                </div>
+                <div class="indexAdver">
+                  <h3 class="content_title">活动&公告</h3>
+                  <a  href="http://www.taiji.tech.com.cn" class="content_detail">太极微服务平台TDF Cloud开始公测</a>
+                </div>
+              </el-tab-pane>
+              <el-tab-pane label="产品功能">
+                产品功能
+              </el-tab-pane>
+              <el-tab-pane label="快速入门">
+                快速入门
+              </el-tab-pane>
+              <el-tab-pane label="开发手册">
+                开发手册
+              </el-tab-pane>
+              <el-tab-pane label="常见问题">
+                常见问题
+              </el-tab-pane>
+            </el-tabs>
+          </el-col>
+        </el-row>
+      </div>
+    </div>
+    <div class="TDF-trait">
+      <h2 align="center">太极微服务平台的特性</h2>
+      <div>
+        <ul class="TraitList">
+          <li v-for="traits in TdfTraits">
+            <span class="traitImg"><img :src="traits.traitImg" width="100%"></span>
+            <span class="traitName">{{ traits.traitName }}</span>
+            <span class="traitdetail"><p>{{ traits.traitDetail }}</p></span>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="indexBottom">
+
+
     </div>
   </div>
 </template>
 
 <script>
-    export default {
-        name: "TdfCloudIndex"
+ export default {
+   name: "TdfCloudIndex",
+   data(){
+     return{
+       TdfTraits:[
+         {
+           traitId:1,
+           traitImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos001.jpg",
+           traitName:'拥抱开源社区',
+           traitDetail:'拥抱Spring Cloud开源社区，提供高可用，可扩展、灵活的微服务社区技术支持，降低用户迁移到微服务的时间金额人力成本。'
+         },{
+           traitId:2,
+           traitImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos001.jpg",
+           traitName:'拥抱开源社区',
+           traitDetail:'拥抱Spring Cloud开源社区，提供高可用，可扩展、灵活的微服务社区技术支持，降低用户迁移到微服务的时间金额人力成本。'
+         },{
+           traitId:3,
+           traitImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos001.jpg",
+           traitName:'拥抱开源社区',
+           traitDetail:'拥抱Spring Cloud开源社区，提供高可用，可扩展、灵活的微服务社区技术支持，降低用户迁移到微服务的时间金额人力成本。'
+         },{
+           traitId:4,
+           traitImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos001.jpg",
+           traitName:'拥抱开源社区',
+           traitDetail:'拥抱Spring Cloud开源社区，提供高可用，可扩展、灵活的微服务社区技术支持，降低用户迁移到微服务的时间金额人力成本。'
+         },{
+           traitId:5,
+           traitImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos001.jpg",
+           traitName:'拥抱开源社区',
+           traitDetail:'拥抱Spring Cloud开源社区，提供高可用，可扩展、灵活的微服务社区技术支持，降低用户迁移到微服务的时间金额人力成本。'
+         },{
+           traitId:6,
+           traitImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos001.jpg",
+           traitName:'拥抱开源社区',
+           traitDetail:'拥抱Spring Cloud开源社区，提供高可用，可扩展、灵活的微服务社区技术支持，降低用户迁移到微服务的时间金额人力成本。'
+         }
+       ]
+     }
+   }
+
+
+
     }
 </script>
 
@@ -31,11 +123,86 @@
   background-color: black;
   height:300px;
   weight:100%;
+  padding-top:1px;
 }
-.banner-font{
-  color:#fff;
-  font-family: 'Microsoft YaHei';
+.div1{
+  height:60px;
+  background-color: aliceblue;
+  margin-top:100px;
+  margin-left:70px;
   font-size:30px;
+  font-family: 'Microsoft YaHei';
+  display:block;
+}
+.div2{
+  height:40px;
+  background-color: aliceblue;
+  margin-top:5px;
+  margin-left:70px;
+  font-size:15px;
+  display:block;
+}
+.button{
+  height:40px;
+  background-color: aliceblue;
+  margin-top:5px;
+  margin-left:70px;
+  display:block;
+}
+.indexPic{
+  background-color: red;
+  height:40px;
+  weight:100%;
+  overflow: hidden;
+  float:left;
+}
+.el-tabs__header{
+  background-color: blueviolet;
+}
+.index{
+  background-color: azure;
+  padding-bottom:50px;
+}
+.content_title{
+  margin-left:65px;
+}
+.content_detail{
+  margin-left:65px;
+  weight:720px;
+}
+.TraitList li{
+  list-style: none;
+  width:28%;
+  border:1px solid #E5E9F2;
+  height: auto;
+  overflow: hidden;
+  background-color:#fff;
+  padding: 10px;
+  float:left;
+  margin: 20px 15px;
+}
+.TraitList li span{
+  display: block;
+  float:left;
+}
+.traitImg{
+  width: 50%;
+}
+.traitName{
+  font-size: 18px;
+  padding-left: 10px;
+  color:brown;
+
+}
+.traitDetail{
+  font-size: 16px;
+  padding-left: 10px;
+  padding-top:10px;
+}
+.indexBottom{
+  background-color: black;
+  height:168px;
+  weight:100%;
 }
 /*.span-font{*/
   /*color: red;*/
